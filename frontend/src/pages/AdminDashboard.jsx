9634bc4faa63218fe2fd65f569830dfff579ca39
bashardev0 <basharharb95@gmail.com>
2025-11-21 16:48:17 +0200

    Refactor comment in AdminDashboard component for clarity

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { Api } from '../api.js';
 import { useAuth } from '../context/AuthContext.jsx';
-//bashar harb tA3A LA NEEEEKKAKKKKKK
+/////ahmad byeklooooooooooooooooooooooooooooooooooooo
 const AdminDashboard = () => {
   const { token } = useAuth();
   const [stats, setStats] = useState(null);
